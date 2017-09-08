b3ea52c1784e5cb28677068c1641c1a1fd6cf028
github username <github email address>
2017-09-08 11:11:29 -0400

    Sikander commit

--- author.rb
+++ author.rb
@@ -1,1 +1,40 @@
+class Author
 
+  attr_accessor :name
+  @count = 0
+  @instances = []
+
+  def initialize(name)
+    @name = name
+    @books = {}
+    @count += 1
+    @instances << self
+  end
+
+  def self.all
+    @instances.inspect
+  end
+
+  def books
+    @books
+  end
+
+  def write_book(title, word_count)
+    book = Book.new(:title, :word_count)
+    @books << book
+    book.author = self
+  end
+
+  def total_words
+    words_array = @books.collect {|k| k[word_count]}
+    words_array.sum
+  end
+
+  def most_words
+    words_array = @books.collect {|x| x[1]}
+    largest_word_count = words_array.sort.last
+
+  end
+
+
+end
--- book.rb
+++ book.rb
@@ -1,1 +1,30 @@
+class Book
 
+  attr_accessor :name, :author
+  @count = 0
+  @instances = []
+
+  def initialize(name, author)
+    @name = name
+    @author = author
+    @count += 1
+    @instances << self
+  end
+
+  def call
+    @author.books
+  end
+
+  def author
+    self.author
+  end
+
+  def title
+    self.title
+  end
+
+  def word_count
+    @author.word_count
+  end
+
+end
